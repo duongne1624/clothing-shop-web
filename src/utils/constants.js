07accb151135d5c6022b3d0539ab0cb42e5ca845
duongne1624 <121754943+duongne1624@users.noreply.github.com>
2025-03-24 12:25:44 +0700

    Fix constants mode for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,7 @@ console.log('import.meta.env: ', import.meta.env)
 if (import.meta.env.MODE === 'development') {
   apiRoot = 'http://localhost:8017'
 }
-if (import.meta.env.BUILD_MODE === 'production') {
+if (import.meta.env.MODE === 'production') {
   apiRoot = 'https://clothing-shop-api-99u0.onrender.com'
 }
-
 export const API_ROOT = apiRoot
